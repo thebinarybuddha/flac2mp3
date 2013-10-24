da072151c939afa1341c653e8db010ce2e5bbdcd
Boyd H. Ako <boyd.hanalei.ako@gmail.com>
2013-10-23 14:52:28 -1000

    Begin code overhaul. Converting code to more PERL based to be crossplatform and use less external programs.

--- flac2mp3.pl
+++ flac2mp3.pl
@@ -1,7 +1,12 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use MP3::Info;
+use MP3::Tag;
+use Audio::FLAC::Header;
 
+
+###	Use METAFLAC to get FLAC tag info
 my $FILE = $ARGV[0];
 my $ARTIST = `metaflac \"$FILE\" --show-tag=ARTIST | sed s/.*=//g`;
 my $TITLE = `metaflac "$FILE" --show-tag=TITLE | sed s/.*=//g`;
@@ -11,6 +16,8 @@ my $TRACKNUM = `metaflac "$FILE" --show-tag=TRACKNUMBER | sed s/.*=//g`;
 my $DATE = `metaflac "$FILE" --show-tag=DATE | sed s/.*=//g`;
 my $OUTF = "$FILE\.mp3";
 
+
+###	Print tag information
 printf "FILE:\t./$FILE\n";
 printf "ARTIST:\t$ARTIST";
 printf "TITLE:\t$TITLE";
@@ -20,5 +27,25 @@ printf "TRACK:\t$TRACKNUM";
 printf "DATE:\t$DATE";
 printf "OUT FILE:\t$OUTF\n";
 
+### convert FLAC file to MP3
 `flac -c -d "$FILE" | lame -q 0 -s 48 --id3v2-only --cbr --preset insane - "$OUTF"`;
-`id3v2 -t "$TITLE" -T "$TRACKNUM" -a "$ARTIST" -A "$ALBUM" -y "$DATE" -2 -g "$GENRE" "$OUTF"`;
+
+
+### Write MP3 tag information
+$mp3 = MP3::Tag->new("$OUTF"); # create object 
+
+$mp3->get_tags(); # read tags
+
+if (exists $mp3->{ID3v1}) { # save track information
+$mp3->{ID3v1}->title("$TITLE");
+$mp3->{ID3v1}->artist("$ARTIST");
+$mp3->{ID3v1}->album("$ALBUM");
+$mp3->{ID3v1}->year("$DATE");
+$mp3->{ID3v1}->write_tag();
+}
+
+$mp3->close(); # destroy object
+
+
+###	Use ID3V2 to write MP3 tags
+#`id3v2 -t "$TITLE" -T "$TRACKNUM" -a "$ARTIST" -A "$ALBUM" -y "$DATE" -2 -g "$GENRE" "$OUTF"`;
